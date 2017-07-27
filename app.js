e650f14aea6c15afdfdb0fd80fc7359ce875e8c3
Sally Bouley <sallybouley87@gmail.com>
2017-07-26 20:37:11 -0600

    remove/next trip buttons

--- app.js
+++ app.js
@@ -1,52 +1,68 @@
 $(document).ready(function(){
-  const baseURL = "https://peaceful-woodland-20463.herokuapp.com/places"
   $.get(baseURL, travelList)
+    .then(deletePlace)
+    .then(nextTrip)
   addPlace()
+
 })
+
+const baseURL = 'https://peaceful-woodland-20463.herokuapp.com/places/';
 
 function travelList(data){
   for (var i=0; i < data.length; i++){
     $('.list').append
     (`<tr>
         <td>${i+1}.</td>
+        <td><a id="${data[i].id}" class="next btn-floating btn-small waves-effect waves-light hoverable blue"><i class="material-icons">add</i></a></td>
         <td>${data[i].city}</td>
         <td>${data[i].state}</td>
-        <td>${data[i].rating}</td>
-        <td><a class="btn-floating btn-small waves-effect waves-light teal lighten-2"><i class="material-icons">mode_edit</i></a>
-            <a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">remove</i></a></td>
+        <td><input id="${data[i].id}" type="number" min="1" max="10" class="center-align" value="${data[i].rating}"></td>
+        <td><a id="${data[i].id}" class="remove btn-floating btn-small waves-effect waves-light hoverable red"><i class="material-icons">remove</i></a></td>
       </tr>`);
   }
+}
+
+function deletePlace() {
+  $('.remove').click(function(){
+    let id = $(this).attr('id');
+    $.ajax({
+      url: baseURL + id,
+      type: 'DELETE',
+      contentType:'application/json'
+    })
+    .then(data => {console.log(data)})
+  })
+
 }
 
 function addPlace(){
   $('#addLocation').click(function(event){
     event.preventDefault();
-    const $city = $('#city').val();
-    const $state = $('#state').val();
-    const $rating = $('#rating').val();
-    console.log($city, $state, $rating)
-    // addItem($city,$state,$rating)
+    let $city = $('#city').val();
+    let $state = $('#state').val();
+    let $rating = parseInt($('#rating').val());
+    addItem($city,$state,$rating)
+    $city = $('#city').val("");
+    $state = $('#state').val("");
+    $rating = $('#rating').val(1);
   })
 }
 
-// function addItem($city,$state,$rating){
-//   const myHeaders = new Headers()
-//   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-//   const setting = {
-//     method: 'Post',
-//     headers: myHeaders,
-//     contentType: 'application/json',
-//     body:$('.list').append
-//         (`<tr>
-//             <td></td>
-//             <td>${$city}</td>
-//             <td>${$state}</td>
-//             <td>${$rating}</td>
-//             <td><a class="btn-floating btn-small waves-effect waves-light teal lighten-2"><i class="material-icons">mode_edit</i></a>
-//                 <a class="btn-floating btn-small waves-effect waves-light red"><i class="material-icons">remove</i></a></td>
-//           </tr>`)
-//   }
-//   $.post(baseURL, {city, state, rating})
-//   fetch(baseURL, setting)
-//   .then(data => data.json())
-// }
+function nextTrip(){
+  $('.next').one("click", function() {
+    let id = $(this).attr('id');
+    $.get(baseURL + id, function(data){
+      $('.nextTrip').append(`<li>${data.city}, ${data.state}</li>`)
+    })
+  })
+}
+
+function addItem(city,state,rating){
+  let post = {
+    city: city,
+    state: state,
+    rating: rating
+  }
+  $.post(baseURL, post)
+  .then(data => {console.log(data)})
+}
