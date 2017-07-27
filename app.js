b825eae46b5eb1baf8a6d2368e28efef57f4e25d
Sally Bouley <sallybouley87@gmail.com>
2017-07-27 15:54:33 -0600

    edit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+  $('.modal').modal();
   $.get(baseURL, travelList)
-    .then(deletePlace)
     .then(nextTrip)
+    .then(deletePlace)
+    .then(updateInfo)
   addPlace()
-
 })
 
 const baseURL = 'https://peaceful-woodland-20463.herokuapp.com/places/';
@@ -11,13 +12,15 @@ const baseURL = 'https://peaceful-woodland-20463.herokuapp.com/places/';
 function travelList(data){
   for (var i=0; i < data.length; i++){
     $('.list').append
-    (`<tr id="r${data[i].id}">
+    (`<tr>
         <td>${i+1}.</td>
         <td><a id="${data[i].id}" class="nextButton btn-floating btn-small waves-effect waves-light hoverable blue"><i class="material-icons">add</i></a></td>
         <td>${data[i].city}</td>
         <td>${data[i].state}</td>
         <td><input id="${data[i].id}" type="number" min="1" max="10" class="center-align" value="${data[i].rating}"></td>
-        <td><a id="${data[i].id}" class="remove btn-floating btn-small waves-effect waves-light hoverable red"><i class="material-icons">clear</i></a></td>
+        <td><a data-target="modal" name="${data[i].id}" class="edit btn-floating btn-small waves-effect waves-light hoverable blue modal-trigger"><i class="material-icons">edit</i></a>
+            <a id="${data[i].id}" class="remove btn-floating btn-small waves-effect waves-light hoverable red"><i class="material-icons">clear</i></a>
+        </td>
       </tr>`);
   }
 }
@@ -30,7 +33,39 @@ function deletePlace() {
       type: 'DELETE',
       contentType:'application/json'
     })
-    .then(data => {console.log(data)})
+    .then(function(){window.location.reload()})
+  })
+}
+
+function updateInfo() {
+  $('.edit').click(function(event){
+    event.preventDefault();
+    let id = $(this).attr('name');
+    $.get(baseURL + id, function(data){
+      $('#modalCity').val(data.city);
+      $('#modalState').val(data.state);
+      $('#modalRating').val(data.rating);
+    })
+    editPlace();
+  })
+}
+
+function editPlace(){
+  $('.modalEdit').click(function(){
+    let id = $(this).attr('name');
+    let edit = {
+      city: $('#modalCity').val(),
+      state: $('#modalState').val(),
+      rating: $('#modalRating').val()
+    }
+
+    $.ajax({
+      url: baseURL + id,
+      type: 'PUT',
+      data: edit,
+      contentType: 'application/json'
+    })
+    .then(addPlace)
   })
 }
 
@@ -63,5 +98,5 @@ function addItem(city,state,rating){
     rating: rating
   }
   $.post(baseURL, post)
-  .then(data => {console.log(data)})
+  .then(function(){window.location.reload()})
 }
